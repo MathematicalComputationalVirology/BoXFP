bd13368c36af59657cfad0c9c126c7e5ce35e801
Sam Clark <sc0271@my.bristol.ac.uk>
2019-09-16 15:18:49 +0100

    create preprocessing script

--- Tutorial/Scripts/final_calcs_v5_1_ms2_pr11.py
+++ Tutorial/Scripts/final_calcs_v5_1_ms2_pr11.py
@@ -31,43 +31,7 @@ if __name__ == '__main__':
     #read in file list
     with open('data_files.fl' ,'r') as f:
         file_list =f.readlines()
-    """
-    #read in data initially to get the SM peak positions
-    data_arr0 = sam_funcs.data_reader(file_list,6000,1400)
-        
-    n=len(data_arr0)
 
-    #plot the size marker traces to ensure that all peaks have been captured
-    color=iter(plt.cm.rainbow(np.linspace(0,1,n)))
-    for i in range(n):
-        col=next(color)
-        plt.plot(data_arr0[i]['Position'],data_arr0[i]['SizeMarker'],c=col)
-    plt.show()
-
-    #run preprocessing
-    data_arr = sam_funcs.preprocess(data_arr0)
-
-    #run mobility shift
-    data_arr1=sam_funcs.mobility_shift(data_arr)
-    
-    #find peaks in TM traces
-    peka,peaksTM=sam_funcs.peak_finder_v2(data_arr1,4,.25,cap=7000)
-    data_arr=[]
-    
-    #list all those peask that have a disproportionate number of SM peaks
-    for i in range(len(peaksTM)):
-        
-        lenTM=len(peaksTM[i])
-        
-        if lenTM!=21:
-            print file_list[i]+' '+str(i)
-    
-    #plot the SM traces with the peak positions marked to make sure the peak finder function has found the right peaks.
-    sam_funcs.sm_plotter(data_arr1,peaksTM,file_list)
-    
-    #run the data reader version two that carries out the windowing and stores the windows in a pickle .obj file
-    sam_funcs.data_reader_v2(file_list,peaksTM,'190902_11')
-    """
     #initialise replicate correlations
     corAB_R_25=[]
     corAC_R_25=[]
