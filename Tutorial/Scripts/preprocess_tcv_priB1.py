e1b38a3a8d2b0dc4666aedae375d343c16120af3
Sam Clark <30653403+PsamClark@users.noreply.github.com>
2021-03-24 12:30:34 +0000

    Update preprocess_tcv_priB1.py
    add timestamping

--- Tutorial/Scripts/preprocess_tcv_priB1.py
+++ Tutorial/Scripts/preprocess_tcv_priB1.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import numpy as np
+import datetime
 
 if __name__ == '__main__':
     
@@ -8,8 +9,14 @@ if __name__ == '__main__':
 
     import BoXFP as xfp
     
+    start = time.time()
+    
     np.set_printoptions(threshold=sys.maxsize)
 
     xfp.RX_preprocess('B1',1350,None,'test',Top=None)
+    
+    end = time.time()
+
+    print 'Runtime is: '+str(end-start)+' seconds')
       
 
