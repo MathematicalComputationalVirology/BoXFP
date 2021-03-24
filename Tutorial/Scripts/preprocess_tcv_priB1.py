57cfda4d8c04092199ab27ac33e943c4510a0f7f
Sam Clark <30653403+PsamClark@users.noreply.github.com>
2021-03-24 12:21:10 +0000

    Update preprocess_tcv_priB1.py
    remove dead code

--- Tutorial/Scripts/preprocess_tcv_priB1.py
+++ Tutorial/Scripts/preprocess_tcv_priB1.py
@@ -1,19 +1,6 @@
 import os
 import sys
 import numpy as np
-import math
-from copy import deepcopy
-import matplotlib.pyplot as plt
-import pandas as pd
-import pickle
-
-
-from scipy import stats
-from random import sample
-from scipy.stats.stats import pearsonr
-
-
-
 
 if __name__ == '__main__':
     
