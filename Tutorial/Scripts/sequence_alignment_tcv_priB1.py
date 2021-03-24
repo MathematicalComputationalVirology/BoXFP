9ab2840ad5dd6571c44b31c8903e80b8777b7e9b
Sam Clark <30653403+PsamClark@users.noreply.github.com>
2021-03-24 12:31:56 +0000

    Update sequence_alignment_tcv_priB1.py
    add timestamping

--- Tutorial/Scripts/sequence_alignment_tcv_priB1.py
+++ Tutorial/Scripts/sequence_alignment_tcv_priB1.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import numpy as np
+import datetime
 
 if __name__ == '__main__':
     
@@ -9,6 +10,14 @@ if __name__ == '__main__':
 
     import BoXFP as xfp
     
+    start = time.time()
+    
     np.set_printoptions(threshold=sys.maxsize)
     
     xfp.RX_position('210315_tcv_B1_0.obj','TCV_ref_genome.fasta',searchStart=2245,searchSpan=10)
+
+    end = time.time()
+
+    print 'Runtime is: '+str(end-start)+' seconds')
+    
+    
